0a8c5a529727af8ea5f84afee3fcead7927a2e97
got2bi <vo5840732@gmail.com>
2024-02-27 09:31:54 +0300

    release functions

--- main.py
+++ main.py
@@ -1,4 +1,57 @@
-from abc import ABCMeta, abstractmethod, abstractproperty
-class Animal():
-    __metaclass__=ABCMeta
-    @
+from abc import ABC, abstractmethod
+class Flying(ABC):
+    @abstractmethod
+    def fly(self):
+        pass
+class Floating(ABC):
+    @abstractmethod
+    def swim(self):
+        pass
+class Running(ABC):
+    @abstractmethod
+    def run(self):
+        pass
+class Animal(ABC):
+    def action():
+        pass
+
+class Ostrich(Animal, Running):
+    def run(self):
+        print("Ostrich is running")
+class Hippo(Animal, Floating, Running):
+    def swim(self):
+        print("Hippo is floating")
+    def run(self):
+        print("Hippo is running")
+class Parrot(Animal, Flying):
+    def fly(self):
+        print("Parrot is flying")
+class Shark(Animal, Floating):
+    def swim(self):
+        print("Shark is floating")
+
+
+ostrich = Ostrich()
+hippo = Hippo()
+parrot = Parrot()
+shark = Shark()
+
+AnimalList = []
+AnimalList.append(ostrich)
+AnimalList.append(hippo)
+AnimalList.append(parrot)
+AnimalList.append(shark)
+
+for Anim in AnimalList:
+    try:
+        Anim.fly()
+    except:
+        pass
+    try:
+        Anim.run()
+    except:
+        pass
+    try:
+        Anim.swim()
+    except:
+        pass
